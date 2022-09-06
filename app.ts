549b1625bb35fc6af02ccaa79c2af6991d3aa502
Ethan Deng <ethand.2091@gmail.com>
2022-09-06 16:51:32 -0700

    query param -> path param

--- app.ts
+++ app.ts
@@ -8,24 +8,11 @@ app.use(express.json());
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
 app.use(cors<Request>());
 
-
-// const testDirection = async () => {
-//   const finalDirection = await convertToMultimodalDirection(await getDefaultTransitDirection("Alton Pkwy 5051", "Donald Bren Hall, Irvine, CA"));
-//   console.log(JSON.stringify(finalDirection, null, 4));
-//   console.log("duration", finalDirection.duration/60,
-//       "\narrival", new Date(finalDirection.arrival_time* 1000).toLocaleString('en-US', {timeZone: 'America/Los_Angeles',}),
-//       "\ndeparture", new Date(finalDirection.departure_time* 1000).toLocaleString('en-US', {timeZone: 'America/Los_Angeles',}));
-// };
-
-
-app.get("/direction", (async (req, res) => {
-    const finalDirection = await getMultiModalDirection(<string>req.query["Origin"], <string>req.query["Destination"]);
+app.get("/direction/:origin/:destination", ( async (req, res) => {
+    const finalDirection = await getMultiModalDirection(req.params["origin"],  req.params["destination"]);
     return res.send(finalDirection);
 }) as RequestHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  // testDirection()
-  //     .then(() => console.log("done"))
-  //     .catch(err => console.log(err));
 });
